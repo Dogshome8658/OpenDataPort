b04e4385d72683deea3f6741a96720b1e69600a9
Dogshome <dogshomegame@gmail.com>
2025-07-04 15:12:58 +0800

    indeed

--- Hoster/linux.c
+++ Hoster/linux.c
@@ -4,8 +4,16 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
+#include<pthread.h>
+
+void lisener(void*psv){
+    printf("Might so");
+    return;
+}
 
 void main(void){
-    printf("hello");
+    pthread_t lisener_id=NULL;
+    pthread_create(&lisener_id,NULL,&lisener,NULL);
+    printf("Shall we?");
     return 0;
 }

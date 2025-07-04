9f2e51d325f7f9f756d344fa193362b89b2bd2ac
Dogshome <dogshomegame@gmail.com>
2025-07-04 15:00:06 +0800

    d

---
+++ Hoster/linux.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+void main(void){
+    printf("hello");
+    return 0;
+}
